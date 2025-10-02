9c6a4bb800b5d70fb17c33b3075a32aee07a5620
Thorinwasher <ragnarlol96@gmail.com>
2025-10-02 18:40:21 +0200

    fix(flaw): Provide proper obscuration level for extent

--- src/main/kotlin/dev/jsinco/recipes/core/flaws/FlawExtent.kt
+++ src/main/kotlin/dev/jsinco/recipes/core/flaws/FlawExtent.kt
@@ -7,21 +7,19 @@ interface FlawExtent {
     fun appliesTo(pos: Int): Boolean
 
     class Everywhere : FlawExtent {
-        override fun obscurationLevel(recipeStepAmount: Int): Double {
-            return recipeStepAmount.toDouble()
-        }
-
-        override fun appliesTo(pos: Int) = true
-    }
-
-    data class WholeStep(val stepIndex: Int) : FlawExtent {
         override fun obscurationLevel(recipeStepAmount: Int) = 1.0
 
         override fun appliesTo(pos: Int) = true
     }
 
+    data class WholeStep(val stepIndex: Int) : FlawExtent {
+        override fun obscurationLevel(recipeStepAmount: Int) = 1.0 / recipeStepAmount
+
+        override fun appliesTo(pos: Int) = true
+    }
+
     data class PartialStep(val stepIndex: Int, val start: Int, val stop: Int) : FlawExtent {
-        override fun obscurationLevel(recipeStepAmount: Int) = 0.5
+        override fun obscurationLevel(recipeStepAmount: Int) = 0.5 / recipeStepAmount
 
         override fun appliesTo(pos: Int): Boolean {
             return start <= pos && pos < stop
