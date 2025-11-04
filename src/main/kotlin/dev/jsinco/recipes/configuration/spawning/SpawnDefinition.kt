2607780afb3074b54510b89b2d0f517e7c2fbbac
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2025-11-04 16:53:43 +0100

    fix(spawn-condition): Start working on listeners

--- src/main/kotlin/dev/jsinco/recipes/configuration/spawning/SpawnDefinition.kt
+++ src/main/kotlin/dev/jsinco/recipes/configuration/spawning/SpawnDefinition.kt
@@ -1,8 +1,8 @@
 package dev.jsinco.recipes.configuration.spawning
 
-import dev.jsinco.recipes.configuration.spawning.ConditionsDefinition
 import dev.jsinco.recipes.configuration.spawning.triggers.TriggersDefinition
 import dev.jsinco.recipes.core.flaws.creation.RecipeViewCreator
+import org.bukkit.inventory.ItemStack
 
 data class SpawnDefinition(
     val enabled: Boolean? = null,
@@ -15,4 +15,9 @@ data class SpawnDefinition(
     val triggers: TriggersDefinition? = null,
     val conditions: ConditionsDefinition? = null,
     val conditionBlacklist: ConditionsDefinition? = null,
-)
+) {
+
+    fun generateItems(): List<ItemStack> {
+
+    }
+}
--- src/main/kotlin/dev/jsinco/recipes/configuration/spawning/triggers/PremadeTrigger.kt
+++ src/main/kotlin/dev/jsinco/recipes/configuration/spawning/triggers/PremadeTrigger.kt
@@ -1,12 +1,13 @@
 package dev.jsinco.recipes.configuration.spawning.triggers
 
+import org.bukkit.entity.EntityType
 import org.bukkit.event.inventory.InventoryType
 
-enum class PremadeTrigger(val spawnTrigger: SpawnTrigger) {
+enum class PremadeTrigger(val spawnTrigger: SpawnTrigger? = null) {
 
     CHEST(InventoryFillTrigger(InventoryType.CHEST)),
     BARREL(InventoryFillTrigger(InventoryType.BARREL)),
-    MINECART(LootSpawnTrigger.fromStrings("chests/abandoned_mineshaft")),
-    FISHING(LootSpawnTrigger.fromStrings("gameplay/fishing/treasure")),
+    MINECART,
+    FISHING,
 
 }
