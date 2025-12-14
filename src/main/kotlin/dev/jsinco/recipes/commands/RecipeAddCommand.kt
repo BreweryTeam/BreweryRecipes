be6e2d7763898861a7761782218fbecb47f75cdc
Mitality <96748385+Mitality@users.noreply.github.com>
2025-12-14 10:55:41 +0100

    feat(commands): Add an optional target argument to `/recipes add [...]`

--- src/main/kotlin/dev/jsinco/recipes/commands/RecipeAddCommand.kt
+++ src/main/kotlin/dev/jsinco/recipes/commands/RecipeAddCommand.kt
@@ -2,6 +2,7 @@ package dev.jsinco.recipes.commands
 
 import com.mojang.brigadier.arguments.DoubleArgumentType
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.context.CommandContext
 import dev.jsinco.recipes.Recipes
 import dev.jsinco.recipes.commands.argument.EnumArgument
 import dev.jsinco.recipes.commands.argument.RecipeArgumentType
@@ -9,36 +10,48 @@ import dev.jsinco.recipes.core.BreweryRecipe
 import dev.jsinco.recipes.core.flaws.creation.RecipeViewCreator
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.command.brigadier.Commands
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes
+import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver
 import org.bukkit.entity.Player
 
 object RecipeAddCommand {
 
-
     fun command(): LiteralArgumentBuilder<CommandSourceStack> {
+
+        fun applyToTargets(context: CommandContext<CommandSourceStack>, targets: Collection<Player>): Int {
+            val recipe = context.getArgument("recipe-key", BreweryRecipe::class.java)
+            val flawLevel = context.getArgument("flaw-level", Double::class.java)
+            val flawType = context.getArgument("flaw-type", RecipeViewCreator.Type::class.java)
+            for (target in targets) {
+                Recipes.recipeViewManager.insertOrMergeView(
+                    target.uniqueId,
+                    recipe.generate(flawLevel, flawType)
+                )
+            }
+            return 1
+        }
+
         return Commands.literal("add")
             .then(
                 Commands.argument("recipe-key", RecipeArgumentType)
                     .then(
-                        Commands.argument(
-                            "flaw-type",
-                            EnumArgument(RecipeViewCreator.Type::class.java)
-                        )
+                        Commands.argument("flaw-type", EnumArgument(RecipeViewCreator.Type::class.java))
                             .then(
-                                Commands.argument(
-                                    "flaw-level", DoubleArgumentType.doubleArg(0.0, 100.0)
-                                ).executes { context ->
-                                    val recipe = context.getArgument("recipe-key", BreweryRecipe::class.java)
-                                    val flawLevel = context.getArgument("flaw-level", Double::class.java)
-                                    val sender = context.source.sender as Player
-                                    Recipes.recipeViewManager.insertOrMergeView(
-                                        sender.uniqueId,
-                                        recipe.generate(
-                                            flawLevel,
-                                            context.getArgument("flaw-type", RecipeViewCreator.Type::class.java)
-                                        )
+                                Commands.argument("flaw-level", DoubleArgumentType.doubleArg(0.0, 100.0))
+                                    .executes { context ->
+                                        val sender = context.source.sender
+                                        if (sender !is Player) return@executes 1
+                                        applyToTargets(context, listOf(sender))
+                                    }
+                                    .then(
+                                        Commands.argument("targets", ArgumentTypes.players())
+                                            .executes { context ->
+                                                val targets = context
+                                                    .getArgument("targets", PlayerSelectorArgumentResolver::class.java)
+                                                    .resolve(context.source)
+                                                applyToTargets(context, targets)
+                                            }
                                     )
-                                    1
-                                }
                             )
                     )
             )
