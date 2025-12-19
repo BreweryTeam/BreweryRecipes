4fda919ec40ae9abd177b4c11ae24b08ffb70551
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2025-12-19 19:34:26 +0100

    fix(migration): Only migrate recipes when permission has been set

--- src/main/kotlin/dev/jsinco/recipes/listeners/MigrationListener.kt
+++ src/main/kotlin/dev/jsinco/recipes/listeners/MigrationListener.kt
@@ -1,6 +1,7 @@
 package dev.jsinco.recipes.listeners
 
 import dev.jsinco.recipes.Recipes
+import net.kyori.adventure.util.TriState
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -28,7 +29,7 @@ class MigrationListener : Listener {
         if (!Recipes.recipesConfig.migrate) return
         for ((recipeId, recipe) in Recipes.recipes()) {
             val permission = permissionTemplate.replace("%recipe%", recipeId)
-            if (!event.player.hasPermission(permission)) continue
+            if (event.player.permissionValue(permission) != TriState.TRUE) continue
             Recipes.recipeViewManager.insertOrMergeView(
                 event.player.uniqueId,
                 recipe.generateCompletedView()
