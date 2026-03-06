f5ce2a9f28b6642bb876ce13bbf820d84e145b4e
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-03-06 17:26:05 +0100

    feat(brew-tracking): Listen to brew tracking, modify brew data

--- src/main/kotlin/dev/jsinco/recipes/listeners/TheBrewingProjectListener.kt
+++ src/main/kotlin/dev/jsinco/recipes/listeners/TheBrewingProjectListener.kt
@@ -1,44 +1,63 @@
 package dev.jsinco.recipes.listeners
 
+import dev.jsinco.brewery.api.brew.Brew
 import dev.jsinco.brewery.api.meta.MetaDataType
 import dev.jsinco.brewery.bukkit.api.TheBrewingProjectApi
 import dev.jsinco.brewery.bukkit.api.event.transaction.BarrelExtractEvent
+import dev.jsinco.brewery.bukkit.api.event.transaction.CauldronExtractEvent
 import dev.jsinco.brewery.bukkit.api.event.transaction.DistilleryExtractEvent
+import dev.jsinco.brewery.bukkit.api.event.transaction.ItemTransactionEvent
+import dev.jsinco.brewery.bukkit.api.transaction.ItemSource
+import dev.jsinco.recipes.util.metadata.UuidMetaDataType
 import net.kyori.adventure.key.Key
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 data class TheBrewingProjectListener(val api: TheBrewingProjectApi) : Listener {
 
     companion object {
-        val COMPLETED_KEY: Key = Key.key("recipes", "completed")
+        val COMPLETED_RECIPE_KEY: Key = Key.key("recipes", "completed_recipe")
+        val COMPLETED_SCORE_KEY: Key = Key.key("recipes", "completed_score")
+        val COMPLETED_BY_KEY: Key = Key.key("recipes", "completed_by")
     }
 
     @EventHandler(ignoreCancelled = true)
     fun onBarrelExtract(event: BarrelExtractEvent) {
-        val result = event.transactionSession.result?.itemStack
-        result ?: return
-        val brew = api.brewManager.fromItem(result).orElse(null) ?: return
-        val recipeKey = (brew.closestRecipe(api.recipeRegistry).orElse(null) ?: return)
-            .recipeName;
-        if (recipeKey == brew.meta(COMPLETED_KEY, MetaDataType.STRING)) {
-            return
-        }
-        val brewModified = brew.withMeta(COMPLETED_KEY, MetaDataType.STRING, recipeKey)
-        // TODO add tracking
+        onInventoryExtract(event, event.player)
     }
 
     @EventHandler(ignoreCancelled = true)
     fun onDistilleryExtract(event: DistilleryExtractEvent) {
-        val result = event.transactionSession.result?.itemStack
-        result ?: return
+        onInventoryExtract(event, event.player)
+    }
+
+    fun onCauldronExtract(event: CauldronExtractEvent) {
+
+    }
+
+    private fun onInventoryExtract(event: ItemTransactionEvent<ItemSource.ItemBasedSource>, player: Player?) {
+        val result = event.transactionSession.result?.itemStack ?: return
         val brew = api.brewManager.fromItem(result).orElse(null) ?: return
-        val recipeKey = (brew.closestRecipe(api.recipeRegistry).orElse(null) ?: return)
-            .recipeName;
-        if (recipeKey == brew.meta(COMPLETED_KEY, MetaDataType.STRING)) {
+        val recipe = brew.closestRecipe(api.recipeRegistry).orElse(null) ?: return
+        val score = brew.score(recipe).score()
+        val recipeKey = recipe.recipeName
+        if (!appliesTo(player, brew, score, recipeKey)) {
             return
         }
-        val brewModified = brew.withMeta(COMPLETED_KEY, MetaDataType.STRING, recipeKey)
-        // TODO add tracking
+        val brewModified = brew.withMeta(COMPLETED_RECIPE_KEY, MetaDataType.STRING, recipeKey)
+            .withMeta(COMPLETED_BY_KEY, MetaDataType.STRING, recipeKey)
+            .withMeta(COMPLETED_SCORE_KEY, MetaDataType.DOUBLE, score)
+        event.transactionSession.result = ItemSource
+            .ItemBasedSource(api.brewManager.toItem(brewModified, Brew.State.Other()))
+    }
+
+    private fun appliesTo(player: Player?, brew: Brew, score: Double, recipeKey: String): Boolean {
+        player ?: return false
+        if ((brew.meta(COMPLETED_BY_KEY, UuidMetaDataType)?.let { player.uniqueId != it }) ?: false) {
+            return false
+        }
+        return recipeKey != brew.meta(COMPLETED_RECIPE_KEY, MetaDataType.STRING) ||
+                (brew.meta(COMPLETED_SCORE_KEY, MetaDataType.DOUBLE) ?: Double.MIN_VALUE) < score
     }
 }
