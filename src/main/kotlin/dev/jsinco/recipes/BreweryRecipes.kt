fe74c8260bcb1868af919d1ed91d837a9e4c315f
Tisawesomeness <tis_dev@protonmail.com>
2026-08-06 22:09:33 -0400

    fix(reload): Don't re-register unchanged recipes on reload

--- src/main/kotlin/dev/jsinco/recipes/BreweryRecipes.kt
+++ src/main/kotlin/dev/jsinco/recipes/BreweryRecipes.kt
@@ -81,7 +81,7 @@ class BreweryRecipes : JavaPlugin() {
         lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) {
             it.registrar().register(RecipesCommand.command())
         }
-        recipesConfig.book.craftingRecipe.register(BookUtil.createBook(), "recipes_book")
+        recipesConfig.book.craftingRecipe.register("recipes_book", BookUtil.createBook())
         spawnConfig.recipeSpawning
             .forEachIndexed { index, definition -> definition.registerRecipe(index) }
         Bukkit.getGlobalRegionScheduler().runAtFixedRate(
@@ -160,6 +160,8 @@ class BreweryRecipes : JavaPlugin() {
     }
 
     fun reload() {
+        val oldBookRecipe = recipesConfig.book.craftingRecipe
+        val oldSpawnDefinitions = spawnConfig.recipeSpawning
         brewingIntegration.reload()
         recipesConfig = readConfig()
         guiConfig = readGuiConfig()
@@ -171,8 +173,12 @@ class BreweryRecipes : JavaPlugin() {
         }
         recipeGuiItemCache.clearGlobal()
         RecipeViewLoreWriter.bumpVersion()
-        recipesConfig.book.craftingRecipe.register(recipesConfig.book.item.generateItem(), "breweryrecipes_book")
+        recipesConfig.book.craftingRecipe.register("recipes_book", BookUtil.createBook(), oldBookRecipe)
         spawnConfig.recipeSpawning
-            .forEachIndexed { index, definition -> definition.registerRecipe(index) }
+            .forEachIndexed { index, definition ->
+                definition.registerRecipe(index, oldSpawnDefinitions.firstOrNull { oldDefinition ->
+                    definition.recipeWhitelist == oldDefinition.recipeWhitelist && definition.recipeBlacklist == oldDefinition.recipeBlacklist
+                })
+            }
     }
 }
